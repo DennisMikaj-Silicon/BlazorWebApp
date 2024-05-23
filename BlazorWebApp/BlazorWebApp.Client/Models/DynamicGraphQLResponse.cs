544a95bb671e75e024d4e62504d1fe9cfe69ff69
Dennis <mikajdennis@gmail.com>
2024-05-23 14:16:46 +0200

    CourseDetails now shows information about the specified course

---
+++ BlazorWebApp/BlazorWebApp.Client/Models/DynamicGraphQLResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorWebApp.Client.Models;
+
+public class DynamicGraphQLResponse
+{
+	[JsonPropertyName("data")]
+	public JsonElement Data { get; set; }
+}
---
+++ BlazorWebApp/BlazorWebApp.Client/Models/GraphQLQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorWebApp.Client.Models;
+
+public class GraphQLQuery
+{
+	public string Query { get; set; } = null!;
+}
